259c75c48e72b26c3e9e054c1d42d6e5a85c37c4
mdmuhim0096 <149795726+mdmuhim0096@users.noreply.github.com>
2025-03-26 15:21:54 +0600

    Update socket.jsx

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,7 +1,7 @@
 // socket.js or similar
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://server-r24b.onrender.com/");
 
 socket.on("connect", () => {
   console.log("Connected to WebSocket server", socket.id);
